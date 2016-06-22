9a40fce41392f153a27e24806e31f925b678710b
arti70 <artemisfowl70@gmail.com>
2016-06-22 13:40:15 -0400

    init

---
+++ js/sideNav.js
@@ -0,0 +1,20 @@
+function updateSideNav(){
+    var navHead = document.getElementById('dynNavId');
+    $('#dynNavId').empty();
+    
+    if(currentCourse == "home"){
+        navHead.innerHTML += "<li class='active'><a id='home_nav_button' href ='#' onclick='showMain();return false;' ><i class='fa fa-fw fa-home' ></i> Home</a></li>";
+    }
+    else{
+        navHead.innerHTML += "<li ><a id='home_nav_button' href ='#' onclick='showMain();return false;' ><i class='fa fa-fw fa-home' ></i> Home</a></li>";
+    }
+    jsonDataObj.forEach(function(e) {
+        if(e.courseName == currentCourse){
+            navHead.innerHTML += "<li class='active'><a id="+ e.id +"href ='#' onclick='updatePage("+ e.index +");return false;' ><i class='fa fa-fw "+e.icon+"' ></i> "+ e.courseName+" "+ e.courseCode+"</a></li>";
+        }
+        else{
+            navHead.innerHTML += "<li><a id="+ e.id +"href ='#' onclick='updatePage("+ e.index +");return false;' ><i class='fa fa-fw "+e.icon+"' ></i> "+ e.courseName+" "+ e.courseCode+"</a></li>";
+        }
+    });
+    console.log("updatesSideNav");
+}
